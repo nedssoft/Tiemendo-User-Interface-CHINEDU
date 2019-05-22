de39c5cc30efe372e31d853edea6ca8df3155516
Orie Chinedu <oriechinedu@gmail.com>
2019-05-22 13:08:51 +0100

    Layout About Us page

--- js/index.js
+++ js/index.js
@@ -1,35 +1,3 @@
-class Nav {
-  constructor(el) {
-    this.nav = el;
-    this.openNavbar();
-    this.checkActiveItem();
-  }
-  openNavbar = () => {
-    const navToggle = this.nav.querySelector('.nav-toggle');
-    const navItemContainer = this.nav.querySelector('.nav-items');
-    navToggle.addEventListener('click', () => {
-      navItemContainer.classList.toggle('open')
-    })
-  }
-  checkActiveItem = () => {
-    const navItems = this.nav.querySelectorAll('.nav-items li');
-    navItems.forEach((item) => {
-      item.addEventListener('click', () => {
-        this.deselect();
-       item.classList.add('active');
-      })
-    })
-  }
-  deselect = () => {
-    const navItems = this.nav.querySelectorAll('.nav-items li');
-    navItems.forEach(item => {
-      item.classList.remove('active');
-    })
-  }
-}
-
-const nav = document.querySelector('.navigation');
-new Nav(nav)
 
 class NavComponent {
   constructor(navData, rootEl) {
